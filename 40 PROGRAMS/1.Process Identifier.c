7e6d6e8b9d85423e0823f19f7e813e8cc2aba385
ashik0072 <114140750+ashik0072@users.noreply.github.com>
2023-02-02 23:58:59 +0000

    Update 1.Process Identifier.c

--- 40 PROGRAMS/1.Process Identifier.c
+++ 40 PROGRAMS/1.Process Identifier.c
@@ -1,1 +1,10 @@
-
+#include<stdio.h>
+#include<unistd.h>
+int main()
+{
+	int processid,parent_processid;
+	processid=getpid();
+	parent_processid=getppid();
+	printf("\nPROCESS ID : %d\n\n",processid);
+	printf("PARENT PROCESS ID : %d",parent_processid);
+}
